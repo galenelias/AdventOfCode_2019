260a9342182a92980a8c08e66e76fc9499db3abe
Galen Elias <gelias@microsoft.com>
2019-12-20 21:00:22 -0800

    Day 20, part 2 in Rust

--- src/day20/mod.rs
+++ src/day20/mod.rs
@@ -17,8 +17,6 @@ fn bfs(grid: &Vec<Vec<char>>, start_pos: &(usize, usize), end_pos: &(usize, usiz
 			return steps;
 		}
 
-		let ch = grid[pos.0][pos.1];
-	
 		let mut try_dir = | new_pos: (usize,  usize) | {
 			if grid[new_pos.0][new_pos.1] == '.' && !visited.contains(&new_pos) {
 				q.push_back((new_pos, steps + 1));
@@ -32,6 +30,50 @@ fn bfs(grid: &Vec<Vec<char>>, start_pos: &(usize, usize), end_pos: &(usize, usiz
 
 		if let Some(portal_out) = portal_pairs.get(&pos) {
 			q.push_back((portal_out.clone(), steps + 1));
+		}
+	}	
+
+	unreachable!("Didn't find exit");
+}
+
+fn is_portal_inner(grid: &Vec<Vec<char>>, portal_pos: &(usize, usize)) -> bool {
+	portal_pos.0 > 2 && portal_pos.0 < grid.len() - 3 && portal_pos.1 > 2 && portal_pos.1 < grid[0].len() - 3
+}
+
+fn bfs2(grid: &Vec<Vec<char>>, start_pos: &(usize, usize), end_pos: &(usize, usize), portal_pairs: &HashMap<(usize, usize), (usize, usize)>) -> usize {
+	// (pos, level, steps)
+	let mut q: VecDeque<((usize, usize), usize, usize)> = VecDeque::new();
+	let mut visited = HashSet::new();
+
+	q.push_back((start_pos.clone(), 0, 0));
+	while !q.is_empty() {
+		let (pos, level, steps) = q.pop_front().unwrap();
+
+		if !visited.insert((pos, level)) {
+			continue;
+		}
+
+		if &pos == end_pos && level == 0 {
+			return steps;
+		}
+
+		let mut try_dir = | new_pos: (usize,  usize) | {
+			if grid[new_pos.0][new_pos.1] == '.' && !visited.contains(&(new_pos, level)) {
+				q.push_back((new_pos, level, steps + 1));
+			}
+		};
+
+		try_dir((pos.0 - 1, pos.1));
+		try_dir((pos.0 + 1, pos.1));
+		try_dir((pos.0, pos.1 - 1));
+		try_dir((pos.0, pos.1 + 1));
+
+		if let Some(portal_out) = portal_pairs.get(&pos) {
+			let is_inner_portal = is_portal_inner(grid, &pos);
+			if is_inner_portal || level > 0 { // Can't take an outer portal if we're on level 0
+				let new_level = if is_inner_portal { level + 1 } else { level - 1 };
+				q.push_back((portal_out.clone(), new_level, steps + 1));
+			}
 		}
 	}	
 
@@ -65,7 +107,7 @@ pub fn solve(inputs : Vec<String>) {
 	}
 
 	let mut portal_pairs: HashMap<(usize, usize), (usize, usize)> = HashMap::new(); // Opening1 => Opening 2
-	for (k, v) in &portals {
+	for (_, v) in &portals {
 		if v.len() == 2 {
 			portal_pairs.insert(v[0].clone(), v[1].clone());
 			portal_pairs.insert(v[1].clone(), v[0].clone());
@@ -75,6 +117,9 @@ pub fn solve(inputs : Vec<String>) {
 	let start = portals.get(&['A', 'A']).unwrap()[0];
 	let end = portals.get(&['Z', 'Z']).unwrap()[0];
 
-	let part1 = bfs(&grid, &start, &end, &portal_pairs);
-	println!("Part 1: {}", part1);
+	// let part1 = bfs(&grid, &start, &end, &portal_pairs);
+	// println!("Part 1: {}", part1);
+
+	let part2 = bfs2(&grid, &start, &end, &portal_pairs);
+	println!("Part 2: {}", part2);
 }
